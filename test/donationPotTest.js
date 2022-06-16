6a0dfc0175b2f4db977748a78a725e7ad775f60c
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2022-06-16 13:31:20 -0400

    :test_tubes: Adds tests for donation and composition

--- test/donationPotTest.js
+++ test/donationPotTest.js
@@ -25,9 +25,54 @@ describe("DonationPot", () => {
         await deployedPot.deployed();
         expect(deployedPot.address).to.exist;
     });
-    it("should allow an investor to add money to the pot", async() => {});
-    it("should allow an investor to add more money to the pot", async() => {});
-    it("should return current pot composition (donor: amount)", async() => {});
+    it("should allow an investor to add money to the pot", async() => {
+        const DonationPot = await ethers.getContractFactory("DonationPot");
+        const deployedPot = await DonationPot.deploy(
+            20000,
+            [...names],
+            [...apiIds],
+            [...accounts]
+        );
+        await deployedPot.deployed();
+        const [owner, donorOne, donorTwo] = await ethers.getSigners();
+        await deployedPot.connect(donorOne).collaborate(100);
+        await deployedPot.connect(donorTwo).collaborate(100);
+        const currentComposition = await deployedPot.currentComposition();
+        expect(Object.keys(currentComposition).length).to.equal(2);
+    });
+    it("should allow an investor to add more money to the pot", async() => {
+        const DonationPot = await ethers.getContractFactory("DonationPot");
+        const deployedPot = await DonationPot.deploy(
+            20000,
+            [...names],
+            [...apiIds],
+            [...accounts]
+        );
+        await deployedPot.deployed();
+        const [owner, donorOne, donorTwo] = await ethers.getSigners();
+        await deployedPot.connect(donorOne).collaborate(100);
+        await deployedPot.connect(donorOne).collaborate(100);
+        const currentComposition = await deployedPot.currentComposition();
+        expect(Object.keys(currentComposition).length).to.equal(1);
+        expect(currentComposition[donorOne.address]).to.equal(200);
+    });
+    it("should return current pot composition {donor: amount}", async() => {
+        const DonationPot = await ethers.getContractFactory("DonationPot");
+        const deployedPot = await DonationPot.deploy(
+            20000,
+            [...names],
+            [...apiIds],
+            [...accounts]
+        );
+        await deployedPot.deployed();
+        const [owner, donorOne, donorTwo] = await ethers.getSigners();
+        await deployedPot.connect(donorOne).collaborate(100);
+        const currentComposition = await deployedPot.currentComposition();
+        expect(Object.keys(currentComposition).length).to.equal(1);
+        expect(currentComposition).to.equal({
+            [donorOne.address]: 100
+        });
+    });
     it("should not allow any voting to occur up until goal amount has been reached", async() => {});
     it("should not allow voting after fifteen (15) days have passed since goal amount was reached", async() => {});
     it("should return voting results after window is over because everyone voted", async() => {});
