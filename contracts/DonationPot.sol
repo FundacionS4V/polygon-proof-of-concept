662979a949a23ad275f013aa059e9ec4682fc82a
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2022-06-16 23:57:26 -0400

    :heavy_check_mark: Passes tests for donors to send eth
    - :test_tube: fixes testing in order to send transaction and get balance through provider.
    
    - :eyes: maybe it's not possible/desirable to build a contract method to return composition...

--- contracts/DonationPot.sol
+++ contracts/DonationPot.sol
@@ -5,30 +5,32 @@ import "hardhat/console.sol";
 
 contract DonationPot {
     // contracts goal amount; when reached vote starts
-    uint immutable goal;
+    uint256 immutable public goal;
+    // a donor with governance over pot funds
+    struct Donor {
+        uint256 funds;
+        bool voted;
+        bool registered;
+    }
     // a humanitarian project looking for funds
     struct Project {
         address payable account;
         string name;
-        uint apiId;
-    }
-    // a donor with governance over pot funds
-    struct Donor {
-        uint funds;
-        uint vote;
+        uint32 apiId;
+        uint16 votes;
     }
     // donor structs
-    mapping(address => Donor) donors;
+    mapping(address => Donor) public donors;
     // project structs
-    mapping(address => Project) choices;
+    mapping(address => Project) public choices;
     // create contract
     // project choices cannot be received as structs
     // as only internal and private functions may
     // receive this type of variables as a parameter
     constructor(
-        uint _goal,
+        uint256 _goal,
         string[] memory _names,
-        uint[] memory _apiIds,
+        uint32[] memory _apiIds,
         address payable[] memory _accounts
     ) {
         // validates all lists have the same amount of values
@@ -37,15 +39,48 @@ contract DonationPot {
             "names, apiIds and accounts arrays must be of same size"
         );
         // sets goal state value
-        goal= _goal;
+        goal = _goal;
         // builds each project with ordered data from lists
         // and adds each one as part of choices structs
-        for (uint index = 0; index < _names.length; index++) {
+        for (uint8 index = 0; index < _names.length; index++) {
             choices[_accounts[index]] = Project(
                 _accounts[index],
                 _names[index],
-                _apiIds[index]
+                _apiIds[index],
+                0
             );
         }
     }
+    receive() external payable {
+        require(msg.value > 0, "can't donate 0 tokens");
+        if (donors[msg.sender].registered) {
+            donors[msg.sender].funds += msg.value;
+        } else {
+            donors[msg.sender].funds = msg.value;
+            donors[msg.sender].voted = false;
+            donors[msg.sender].registered = true;
+        }
+    }
+    function getDonationsBy(address _donorAddress) 
+        external 
+        view 
+        returns(uint256 _donation) {
+        require(donors[_donorAddress].registered, "haven't donated anything yet");
+        return donors[_donorAddress].funds;
+    }
+    // function sendTransfer(address payable _to, uint256 _amount) external payable {
+    //     _to.transfer(_amount);
+    // }
+    // function collaborate() public payable returns(uint256 currentDonorFunds) {
+    //     require(msg.value > 0, "can't donate 0 tokens");
+    //     console.log(msg.value);
+    //     if (donors[msg.sender].registered) {
+    //         donors[msg.sender].funds += msg.value;
+    //     } else {
+    //         donors[msg.sender].funds = msg.value;
+    //         donors[msg.sender].vote = 0;
+    //         donors[msg.sender].registered = true;
+    //     }
+    //     return donors[msg.sender].funds;
+    // }
 }
--- test/donationPotTest.js
+++ test/donationPotTest.js
@@ -1,10 +1,11 @@
 const { expect } = require("chai");
-const { MockProvider } = require("ethereum-waffle");
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
 
 describe("DonationPot", () => {
-    // create projects with name, api id and wallet address
-    const provider = new MockProvider();
+    let DonationPot;
+    let deployedPot;
+    let donors;
+    const provider = waffle.provider;
     const wallets = provider.getWallets();
     const accounts = [];
     const names = [];
@@ -14,65 +15,47 @@ describe("DonationPot", () => {
         apiIds.push(index + 1);
         accounts.push(wallets[index].address);
     }
-    it("should be created based on voting choices available and goal amount", async() => {
-        const DonationPot = await ethers.getContractFactory("DonationPot");
-        const deployedPot = await DonationPot.deploy(
+    beforeEach(async function () {
+        DonationPot = await ethers.getContractFactory("DonationPot");
+        deployedPot = await DonationPot.deploy(
             20000,
             [...names],
             [...apiIds],
             [...accounts]
         );
         await deployedPot.deployed();
-        expect(deployedPot.address).to.exist;
+        donors = await ethers.getSigners();
     });
-    it("should allow an investor to add money to the pot", async() => {
-        const DonationPot = await ethers.getContractFactory("DonationPot");
-        const deployedPot = await DonationPot.deploy(
-            20000,
-            [...names],
-            [...apiIds],
-            [...accounts]
-        );
-        await deployedPot.deployed();
-        const [owner, donorOne, donorTwo] = await ethers.getSigners();
-        await deployedPot.connect(donorOne).collaborate(100);
-        await deployedPot.connect(donorTwo).collaborate(100);
-        const currentComposition = await deployedPot.currentComposition();
-        expect(Object.keys(currentComposition).length).to.equal(2);
+    it("should be created based on voting choices available and goal amount", async() => {
+        expect(await provider.getBalance(deployedPot.address)).to.equal(0);
+    });
+    it("should allow a donor to add money to the pot", async() => {
+        const tx = await donors[1].sendTransaction({
+            to: deployedPot.address,
+            value: 100
+        });
+        await tx.wait();
+        expect(await provider.getBalance(deployedPot.address)).to.equal(100);
+        expect(await deployedPot.getDonationsBy(donors[1].address)).to.equal(100);
     });
     it("should allow an investor to add more money to the pot", async() => {
-        const DonationPot = await ethers.getContractFactory("DonationPot");
-        const deployedPot = await DonationPot.deploy(
-            20000,
-            [...names],
-            [...apiIds],
-            [...accounts]
-        );
-        await deployedPot.deployed();
-        const [owner, donorOne, donorTwo] = await ethers.getSigners();
-        await deployedPot.connect(donorOne).collaborate(100);
-        await deployedPot.connect(donorOne).collaborate(100);
-        const currentComposition = await deployedPot.currentComposition();
-        expect(Object.keys(currentComposition).length).to.equal(1);
-        expect(currentComposition[donorOne.address]).to.equal(200);
-    });
-    it("should return current pot composition {donor: amount}", async() => {
-        const DonationPot = await ethers.getContractFactory("DonationPot");
-        const deployedPot = await DonationPot.deploy(
-            20000,
-            [...names],
-            [...apiIds],
-            [...accounts]
-        );
-        await deployedPot.deployed();
-        const [owner, donorOne, donorTwo] = await ethers.getSigners();
-        await deployedPot.connect(donorOne).collaborate(100);
-        const currentComposition = await deployedPot.currentComposition();
-        expect(Object.keys(currentComposition).length).to.equal(1);
-        expect(currentComposition).to.equal({
-            [donorOne.address]: 100
+        let tx = await donors[1].sendTransaction({
+            to: deployedPot.address,
+            value: 100
         });
+        tx = await donors[2].sendTransaction({
+            to: deployedPot.address,
+            value: 100
+        });
+        tx = await donors[2].sendTransaction({
+            to: deployedPot.address,
+            value: 100
+        });
+        await tx.wait();
+        expect(await provider.getBalance(deployedPot.address)).to.equal(300);
+        expect(await deployedPot.getDonationsBy(donors[2].address)).to.equal(200);
     });
+    it("should return current pot composition {donor: amount}", async() => {});
     it("should not allow any voting to occur up until goal amount has been reached", async() => {});
     it("should not allow voting after fifteen (15) days have passed since goal amount was reached", async() => {});
     it("should return voting results after window is over because everyone voted", async() => {});
