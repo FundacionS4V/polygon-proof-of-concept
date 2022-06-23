a80536af89f298be8f4738243b001192d6c5d707
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2022-06-23 14:44:13 -0400

    :heavy_check_mark: Passes test for funds being transfered to winner

--- test/donationPotTest.js
+++ test/donationPotTest.js
@@ -30,43 +30,43 @@ describe("DonationPot", () => {
         expect(await provider.getBalance(deployedPot.address)).to.equal(0);
     });
     it("should allow a donor to add money to the pot", async() => {
-        const tx = await donors[1].sendTransaction({
+        const tx = await donors[4].sendTransaction({
             to: deployedPot.address,
             value: 100
         });
         await tx.wait();
         expect(await provider.getBalance(deployedPot.address)).to.equal(100);
-        expect(await deployedPot.getDonationsFrom(donors[1].address)).to.equal(100);
+        expect(await deployedPot.getDonationsFrom(donors[4].address)).to.equal(100);
     });
     it("should allow an investor to add more money to the pot", async() => {
-        let tx = await donors[1].sendTransaction({
+        let tx = await donors[4].sendTransaction({
             to: deployedPot.address,
             value: 100
         });
-        tx = await donors[2].sendTransaction({
+        tx = await donors[5].sendTransaction({
             to: deployedPot.address,
             value: 100
         });
-        tx = await donors[2].sendTransaction({
+        tx = await donors[5].sendTransaction({
             to: deployedPot.address,
             value: 100
         });
         await tx.wait();
         expect(await provider.getBalance(deployedPot.address)).to.equal(300);
-        expect(await deployedPot.getDonationsFrom(donors[2].address)).to.equal(200);
+        expect(await deployedPot.getDonationsFrom(donors[5].address)).to.equal(200);
     });
     it("should return donors and donations in order to build current pot composition {donor: donation}", async() => {
         const donationObjects = {
-            [donors[1].address]: {
-                signer: donors[1],
+            [donors[4].address]: {
+                signer: donors[4],
                 donation: 25
             },
-            [donors[2].address]: {
-                signer: donors[2],
+            [donors[5].address]: {
+                signer: donors[5],
                 donation: 100
             },
-            [donors[3].address]: {
-                signer: donors[3],
+            [donors[6].address]: {
+                signer: donors[6],
                 donation: 431
             }
         };
@@ -88,34 +88,34 @@ describe("DonationPot", () => {
         }
     });
     it("should not allow any voting to occur up until goal amount has been reached", async() => {
-        let tx = await donors[1].sendTransaction({
+        let tx = await donors[4].sendTransaction({
             to: deployedPot.address,
             value: 100
         });
         await tx.wait();
-        await expect(deployedPot.connect(donors[1]).vote(2)).to.be.revertedWith("pot goal has not been reached; no voting allowed yet.");
+        await expect(deployedPot.connect(donors[4]).vote(2)).to.be.revertedWith("pot goal has not been reached; no voting allowed yet.");
     });
     it("should not allow voting after fifteen (15) days have passed since goal amount was reached", async() => {
-        let tx = await donors[1].sendTransaction({
+        let tx = await donors[4].sendTransaction({
             to: deployedPot.address,
             value: 21000
         });
         await tx.wait();
         await provider.send('evm_increaseTime', [15 * 24 * 60 * 60]);
         await provider.send('evm_mine');
-        await expect(deployedPot.connect(donors[1]).vote(1)).to.be.revertedWith("sorry, voting window is closed.");
+        await expect(deployedPot.connect(donors[4]).vote(1)).to.be.revertedWith("sorry, voting window is closed.");
     });
     it("should return voting results after window is over because everyone voted", async() => {
         const donationObjects = [{
-            signer: donors[1],
+            signer: donors[4],
             donation: 2500,
             vote: 1,
         }, {
-            signer: donors[2],
+            signer: donors[5],
             donation: 10000,
             vote: 1
         }, {
-            signer: donors[3],
+            signer: donors[6],
             donation: 43100,
             vote: 3
         }];
@@ -140,15 +140,15 @@ describe("DonationPot", () => {
     });
     it("should return voting results after window is over because voting deadline is reached", async() => {
         const donationObjects = [{
-            signer: donors[1],
+            signer: donors[4],
             donation: 2500,
             vote: 1,
         }, {
-            signer: donors[2],
+            signer: donors[5],
             donation: 10000,
             vote: 1
         }, {
-            signer: donors[3],
+            signer: donors[6],
             donation: 43100,
             vote: 3
         }];
@@ -178,5 +178,40 @@ describe("DonationPot", () => {
         expect(winnerAddress).to.equal(wallets[0].address);
         expect(transferedFunds).to.equal(55600);
     });
-    it("should send pot money to winning choice wallet right after vote has ended", async() => {});
+    it("should send pot money to winning choice wallet right after vote has ended", async() => {
+        const initialBalance = await provider.getBalance(wallets[2].address);
+        const donationObjects = [{
+            signer: donors[4],
+            donation: 2500,
+            vote: 1,
+        }, {
+            signer: donors[5],
+            donation: 10000,
+            vote: 1
+        }, {
+            signer: donors[6],
+            donation: 43100,
+            vote: 3
+        }];
+        let tx;
+        for (let donationObject of donationObjects) {
+            tx = await donationObject.signer.sendTransaction({
+                to: deployedPot.address,
+                value: donationObject.donation
+            });
+        }
+        await tx.wait();
+        for (let donationObject of donationObjects) {
+            tx = await deployedPot.connect(
+                donationObject.signer
+            ).vote(donationObject.vote);
+        }
+        await tx.wait();
+        const [winnerId, winnerAddress, transferedFunds] = await deployedPot.getWinner();
+        expect(winnerId).to.equal(3);
+        expect(winnerAddress).to.equal(wallets[2].address);
+        expect(transferedFunds).to.equal(55600);
+        const finalBalance = await provider.getBalance(winnerAddress);
+        expect(finalBalance.sub(initialBalance).toNumber()).to.equal(55600);
+    });
 });
